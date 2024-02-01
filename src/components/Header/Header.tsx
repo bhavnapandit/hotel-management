95cae046bc8d68d955a68c6e976496d0b191eb60
bhavnapandit <pritybhavna2001@gmail.com>
2024-02-01 12:22:20 +0530

    add

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,17 @@
+"use client";
+import ThemeContext from "@/context/themeContext";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useContext } from "react";
 import { FaUserCircle } from "react-icons/fa";
-import { MdDarkMode } from "react-icons/md";
+import { MdDarkMode, MdOutlineLightMode } from "react-icons/md";
 
 const Header = () => {
+  const { darkTheme, setDarkTheme } = useContext(ThemeContext);
+
+  const { data: session } = useSession();
+
   return (
     <header className="py-10 px-4 mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
       <div className="flex items-center w-full md:w-2/3">
@@ -12,26 +20,60 @@ const Header = () => {
         </Link>
         <ul className="flex items-center ml-5">
           <li className="flex items-center">
-            <Link href="/auth">
-              <FaUserCircle className="cursor-pointer" />
-            </Link>
+            {session?.user ? (
+              <Link href={`/users/${session.user.id}`}>
+                {session.user.image ? (
+                  <div className="w-10 h-10 rounded-full overflow-hidden">
+                    <Image
+                      src={session.user.image}
+                      alt={session.user.name!}
+                      width={40}
+                      height={40}
+                      className="scale-animation img"
+                    />
+                  </div>
+                ) : (
+                  <FaUserCircle className="cursor-pointer" />
+                )}
+              </Link>
+            ) : (
+              <Link href="/auth">
+                <FaUserCircle className="cursor-pointer" />
+              </Link>
+            )}
           </li>
 
           <li className="ml-2">
-            <MdDarkMode className="cursor-pointer" />
+            {darkTheme ? (
+              <MdOutlineLightMode
+                className="cursor-pointer"
+                onClick={() => {
+                  setDarkTheme(false);
+                  localStorage.removeItem("hotel-theme");
+                }}
+              />
+            ) : (
+              <MdDarkMode
+                className="cursor-pointer"
+                onClick={() => {
+                  setDarkTheme(true);
+                  localStorage.setItem("hotel-theme", "true");
+                }}
+              />
+            )}
           </li>
         </ul>
       </div>
 
       <ul className="flex items-center justify-between w-full md:w-1/3 mt-4">
         <li className="hover:-translate-y-2 duration-500 transition-all">
-            <Link href="/">Home</Link>
+          <Link href="/">Home</Link>
         </li>
         <li className="hover:-translate-y-2 duration-500 transition-all">
-            <Link href="/rooms">Rooms</Link>
+          <Link href="/rooms">Rooms</Link>
         </li>
         <li className="hover:-translate-y-2 duration-500 transition-all">
-            <Link href="/contact">Contact</Link>
+          <Link href="/contact">Contact</Link>
         </li>
       </ul>
     </header>
