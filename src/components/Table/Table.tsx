00e995aac1aa1a6d917d7623fc2c4daeb33ce261
bhavnapandit <pritybhavna2001@gmail.com>
2024-02-09 22:54:19 +0530

    add

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,29 +1,34 @@
+'use client';
+
+import { Dispatch, FC, SetStateAction } from 'react';
+import { useRouter } from 'next/navigation';
+
 import { Bookings } from '@/models/booking';
-import { useRouter } from 'next/router';
-import React, { Dispatch, FC, SetStateAction } from 'react'
 
 type Props = {
-    bookingDetails: Bookings[];
-    setRoomId: Dispatch<SetStateAction<string | null>>;
-  };
+  bookingDetails: Bookings[];
+  setRoomId: Dispatch<SetStateAction<string | null>>;
+  toggleRatingModal: () => void;
+};
+
+const Table: FC<Props> = ({ bookingDetails, setRoomId, toggleRatingModal }) => {
+  const router = useRouter();
   
-const Table :FC<Props>=({bookingDetails,setRoomId}) => {
-    const router = useRouter();
   return (
     <div className='overflow-x-auto max-w-[340px] rounded-lg mx-auto md:max-w-full shadow-md sm:rounded-lg'>
-    <table className='w-full text-sm text-left text-gray-500'>
-      <thead className='text-xs text-gray-700 uppercase bg-gray-50'>
-        <tr>
-          <th className='px-6 py-3'>Room name</th>
-          <th className='px-6 py-3'>Unit Price</th>
-          <th className='px-6 py-3'>Price</th>
-          <th className='px-6 py-3'>Discount</th>
-          <th className='px-6 py-3'>No. Days Booked</th>
-          <th className='px-6 py-3'>Days Left</th>
-          <th className='px-6 py-3'></th>
-        </tr>
-      </thead>
-      <tbody>
+      <table className='w-full text-sm text-left text-gray-500'>
+        <thead className='text-xs text-gray-700 uppercase bg-gray-50'>
+          <tr>
+            <th className='px-6 py-3'>Room name</th>
+            <th className='px-6 py-3'>Unit Price</th>
+            <th className='px-6 py-3'>Price</th>
+            <th className='px-6 py-3'>Discount</th>
+            <th className='px-6 py-3'>No. Days Booked</th>
+            <th className='px-6 py-3'>Days Left</th>
+            <th className='px-6 py-3'></th>
+          </tr>
+        </thead>
+        <tbody>
           {bookingDetails.map(booking => (
             <tr
               key={booking._id}
@@ -44,9 +49,10 @@ const Table :FC<Props>=({bookingDetails,setRoomId}) => {
               <td className='px-6 py-4'>0</td>
               <td className='px-6 py-4'>
                 <button
-                  onClick={() => {
-                    setRoomId(booking.hotelRoom._id);
-                  }}
+                  // onClick={() => {
+                  //   setRoomId(booking.hotelRoom._id);
+                  //   toggleRatingModal()
+                  // }}
                   className='font-medium text-blue-600 hover:underline'
                 >
                   Rate
@@ -55,9 +61,9 @@ const Table :FC<Props>=({bookingDetails,setRoomId}) => {
             </tr>
           ))}
         </tbody>
-    </table>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default Table
+export default Table;
